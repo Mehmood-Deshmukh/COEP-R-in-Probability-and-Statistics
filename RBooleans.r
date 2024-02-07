978a2661b063e1e01ce3f089d20f051ad2289ac7
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-02-07 21:02:46 +0530

    Added String, Escape Characters, Boolean

---
+++ RBooleans.r
@@ -0,0 +1,5 @@
+In programming, you often need to know if an expression is true or false.
+
+You can evaluate any expression in R, and get one of two answers, TRUE or FALSE.
+
+When you compare two values, the expression is evaluated and R returns the logical answer:
---
+++ RString.r
@@ -0,0 +1,7 @@
+# Strings are used for storing text.
+
+# A string is surrounded by either single quotation marks, or double quotation marks:
+
+# "hello" is the same as 'hello':
+"hello"
+'hello'
